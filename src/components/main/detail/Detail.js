6fc48061bcd726d1b15426028f03abb679dcfb9a
tikken <tikken23@gmail.com>
2019-03-19 19:59:06 +0300

    descriptionDone

--- src/components/main/detail/Detail.js
+++ src/components/main/detail/Detail.js
@@ -3,7 +3,7 @@ import User from '../../../assets/img/user-1.jpg'
 import User2 from '../../../assets/img/user-2.jpg'
 import User3 from '../../../assets/img/user-3.jpg'
 import User4 from '../../../assets/img/user-4.jpg'
-
+import arrow from '../../../assets/icomoon/SVG/chevron-thin-right.svg'
 
 export default function Detail() {
   return (
@@ -16,14 +16,31 @@ export default function Detail() {
           Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate delectus, inventore iure aliquid aliquam.
         </p>
         <ul className="list">
-            <li className="list__item">Close to the beach</li>
-            <li className="list__item">Breakfast included</li>
-            <li className="list__item">Free airport shuttle</li>
-            <li className="list__item">Free wifi in all rooms</li>
-            <li className="list__item">Air conditioning and heating</li>
-            <li className="list__item">Pets allowed</li>
-            <li className="list__item">We speak all languages</li>
-            <li className="list__item">Perfect for families</li>
+            <li className="list__item">
+              <img src={arrow} className="list__item-icon"/>
+              Close to the beach  
+            </li>
+            <li className="list__item">
+              <img src={arrow} className="list__item-icon"/>                         
+              Breakfast included</li>
+            <li className="list__item">
+            <img src={arrow} className="list__item-icon"/>                                       
+              Free airport shuttle</li>
+            <li className="list__item">
+            <img src={arrow} className="list__item-icon"/>                                       
+              Free wifi in all rooms</li>
+            <li className="list__item">
+            <img src={arrow} className="list__item-icon"/>                                     
+              Air conditioning and heating</li>
+            <li className="list__item">
+            <img src={arrow} className="list__item-icon"/>                                       
+              Pets allowed</li>
+            <li className="list__item">
+            <img src={arrow} className="list__item-icon"/>                                     
+              We speak all languages</li>
+            <li className="list__item">
+            <img src={arrow} className="list__item-icon"/>                                     
+              Perfect for families</li>
         </ul>
 
         <div className="recommend">
